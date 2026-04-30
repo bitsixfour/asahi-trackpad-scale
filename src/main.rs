ac285ba9c1a9d897095c1811f65b40648449a32f
will <me@wngyn.net>
2026-04-29 19:55:39 -0500

    dsklfjklsjdf

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let status: &str = trackpad.name().unwrap_or("I don't know");
 
     println!("starting function on {}", status);
-    println!("vat is the supported inputs: {:?}", trackpad.get_abs_state());
-    for ev in trackpad.fetch_events()? {
-         if let EventSummary::AbsoluteAxis(_, AbsoluteAxisCode::ABS_PRESSURE, value) =
-       ev.destructure() {
-             println!("pressure = {value}"); // prints 12 for your example
-         }
-    }
 
+    loop {
+        for ev in trackpad.fetch_events()? {
+            if let EventSummary::AbsoluteAxis(_, AbsoluteAxisCode::ABS_MT_PRESSURE, value) =
+            ev.destructure() {
+                println!("pressure = {value}"); // prints 12 for your example
+            }
+        }
+    }
     Ok(())
 }
